7a53eeec2b47518a5d566f639da362c492b5afca
Fuad Murad <murad.fuad24@gmail.com>
2025-08-18 19:57:11 -0300

    Upando o projeto git

---
+++ src/main/java/Main.java
@@ -0,0 +1,26 @@
+import json.JsonService;
+import information.Game;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class Main {
+    public static void main(String[] args) {
+
+        Game game1 = Game.builder().name("Expedition 33").music("Lumiere").build();
+        Game game2 = Game.builder().name("Elden ring").music("Main theme").build();
+
+        List<Game> games = new ArrayList<>();
+
+        games.add(game1);
+        games.add(game2);
+
+        JsonService jsonService = new JsonService();
+        String json = jsonService.convertToJson(games);
+
+        System.out.println(json);
+        jsonService.saveJson(json);
+
+    }
+}
