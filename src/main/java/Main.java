8154faad3ad66af77f5bf2260e8102188d46b1a7
Gabriel Alves Pereira <gab.alves.pereira30@gmail.com>
2025-09-01 20:30:47 -0300

    feat: add a new options in Game.java and add more information in game in main.java

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -11,13 +11,15 @@ public class Main {
         Game game1 = Game.builder().name("Expedition 33").music("Lumiere").build();
         Game game2 = Game.builder().name("Elden ring").music("Main theme").build();
         Game game3 = Game.builder().name("STAR WARS: A GUERRA DOS CLONES").music("DUEL OF FATES").build();
-        Game game4 = Game.builder().name("Hades").music("Main theme").build(); 
+        Game game4 = Game.builder().name("Hades").music("Main theme").difficulty("HARD").MetaScore(8.0).Year(2016).build();
+        Game game5 = Game.builder().name("Teste").build();
 
         List<Game> games = new ArrayList<>();
 
         games.add(game1);
         games.add(game2);
         games.add(game3);
+        games.add(game4);
 
         JsonService jsonService = new JsonService();
         String json = jsonService.convertToJson(games);
