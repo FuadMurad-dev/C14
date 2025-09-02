dd1011574fa7d33af929596f762d40c9cca0a78d
Gabriel Alves Pereira <145914573+Gab-Zag@users.noreply.github.com>
2025-09-02 17:06:43 -0300

    refactor(main.java): refactoring main.java so that the tests run feat(gameGadesTest.java): Creating unit tests for the game Hades

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,29 +4,31 @@ import information.Game;
 import java.util.ArrayList;
 import java.util.List;
 
-
 public class Main {
+
+    private static final List<Game> games = new ArrayList<>();
+
     public static void main(String[] args) {
-
-        Game game1 = Game.builder().name("Expedition 33").music("Lumiere").difficulty("HARD").MetaScore(99).Year(2025).build();
-        Game game2 = Game.builder().name("Elden ring").music("Main theme").difficulty("HARD").MetaScore(95).Year(2022).build();
-        Game game3 = Game.builder().name("STAR WARS: A GUERRA DOS CLONES").music("DUEL OF FATES").difficulty("HARD").MetaScore(88).Year(2008).build();
-        Game game4 = Game.builder().name("Hades").music("Main theme").difficulty("HARD").MetaScore(90).Year(2016).build();
-        Game game5 = Game.builder().name("Person 5").music("I belive").difficulty("Median").MetaScore(70).Year(2019).build();
-
-        List<Game> games = new ArrayList<>();
-
-        games.add(game1);
-        games.add(game2);
-        games.add(game3);
-        games.add(game4);
-        games.add(game5);
+        initializeGames();
 
         JsonService jsonService = new JsonService();
         String json = jsonService.convertToJson(games);
 
         System.out.println(json);
         jsonService.saveJson(json);
+    }
 
+    public static void initializeGames() {
+        if (!games.isEmpty()) return;
+
+        games.add(Game.builder().name("Expedition 33").music("Lumiere").difficulty("HARD").MetaScore(99).Year(2025).build());
+        games.add(Game.builder().name("Elden ring").music("Main theme").difficulty("HARD").MetaScore(95).Year(2022).build());
+        games.add(Game.builder().name("STAR WARS: A GUERRA DOS CLONES").music("DUEL OF FATES").difficulty("HARD").MetaScore(88).Year(2008).build());
+        games.add(Game.builder().name("Hades").music("Main theme").difficulty("HARD").MetaScore(90).Year(2016).build());
+        games.add(Game.builder().name("Person 5").music("I belive").difficulty("Median").MetaScore(70).Year(2019).build());
+    }
+
+    public static List<Game> getGames() {
+        return games;
     }
 }
